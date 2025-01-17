b9f5127019c4a51fa90d83b6e45047ceb47b6de9
Matheus <matheusvargas042@gmail.com>
2025-01-16 22:20:43 -0300

    feat: creating property model

--- imoveis_api/models.py
+++ imoveis_api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, Integer, String, func
+from sqlalchemy import Column, DateTime, Float, Integer, String, func
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,3 +12,19 @@ class User(Base):
     password = Column(String)
     email = Column(String, unique=True)
     created_at = Column(DateTime, server_default=func.now())
+
+
+class Property(Base):
+    __tablename__ = 'properties'
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
+    size = Column(String)
+    rooms = Column(Integer)
+    bathrooms = Column(Integer)
+    garages = Column(Integer)
+    value = Column(Float)
+    description = Column(String)
+    address = Column(String)
+    city = Column(String)
+    state = Column(String)
+    status = Column(String)
