30b38bdc08cbf2661ccb23f9283a1cd9e1631588
Matheus <matheusvargas042@gmail.com>
2025-02-18 19:09:26 -0300

    test: testing security.py

--- imoveis_api/app.py
+++ imoveis_api/app.py
@@ -2,13 +2,14 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from imoveis_api.routers import properties, users
+from imoveis_api.routers import auth, properties, users
 from imoveis_api.schemas import Message
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(properties.router)
+app.include_router(auth.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
